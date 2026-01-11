25196a3cf81e4aa8dc2257a7ab0bafbb5dcefc87
tolatolatop <tolatolatop@gmail.com>
2026-01-11 19:03:00 +0000

    feat: 添加wizard模型

---
+++ src/agent/role/wizard.py
@@ -0,0 +1,21 @@
+from typing import TypedDict
+
+
+class TaskState(TypedDict):
+    pass
+
+
+class PlanState(TypedDict):
+    pass
+
+
+class NoteState(TypedDict):
+    pass
+
+
+class AgentState(TypedDict):
+    pass
+
+
+class WizardState(TypedDict):
+    pass
