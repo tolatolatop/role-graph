63f27e2a97308df5915f420ba73e163578ebcba7
tolatolatop <tolatolatop@gmail.com>
2026-01-12 17:49:46 +0000

    feat: gemini特殊格式不适合演示用openai替代

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -4,6 +4,7 @@ import os
 
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 
 from agent.tools import tools
 
@@ -11,7 +12,12 @@ from agent.tools import tools
 def create_custom_agent():
     """Create an agent for a user."""
     load_dotenv()
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash", api_key=os.getenv("GEMINI_API_KEY")
+    # llm = ChatGoogleGenerativeAI(
+    #     model="gemini-2.5-flash", api_key=os.getenv("GEMINI_API_KEY")
+    # )
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_BASE_URL"),
     )
     return llm.bind_tools(tools)
